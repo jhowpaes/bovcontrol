fec8faac3cd830ea05e1fedd8ff11ca38515fc4e
Jhow Paes <jhowylli@gmail.com>
2023-04-23 13:58:32 -0300

    files prebuild generate

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,17 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'babel-plugin-module-resolver',
+        {
+          extensions: ['.ts', '.tsx', '.js', '.ios.js', '.android.js'],
+          alias: {
+            '@core': './src/core',
+            '@modules': './src/modules',
+          },
+        },
+      ],
+    ],
   };
 };
